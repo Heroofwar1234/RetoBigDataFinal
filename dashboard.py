58141ebf7276fa3722baca2ab464c90dcea2d40a
Heroofwar1234 <a01784773@tec.mx>
2025-03-24 20:11:39 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,10 @@ try:
     st.write(f"### People Born in {selected_year}")
     st.dataframe(filtered_df)
 
-   
+   st.write("Sex Distribution")
+    df_pandas = df.toPandas()  # Convert Spark DataFrame to Pandas DataFrame
+    gender_counts = df_pandas['sex'].value_counts()  # Group and count occurrences of "sex"
+    st.bar_chart(gender_counts)  # Display the bar chart
 
 except FileNotFoundError:
     st.error(f"Processed data `{DATA_PATH}` not found! Run `people.py` first.")
